730069a1228c72f7fbc24f89215d68f5854eb709
Evgeniy Erokhin <evgeniierokhin@proton.me>
2022-12-09 15:04:53 +0300

    Edited class InfoMessage

--- homework.py
+++ homework.py
@@ -1,5 +1,5 @@
 from typing import Type, List, Dict
-from dataclasses import dataclass
+from dataclasses import dataclass, asdict
 
 
 @dataclass
@@ -10,13 +10,15 @@ class InfoMessage:
     distance: float
     speed: float
     calories: float
+    INFO = (
+        'Тип тренировки: {training_type}; '
+        'Длительность: {duration:.3f} ч.; '
+        'Дистанция: {distance:.3f} км; '
+        'Ср. скорость: {speed:.3f} км/ч; '
+        'Потрачено ккал: {calories:.3f}.')
 
     def get_message(self) -> str:
-        return (f'Тип тренировки: {self.training_type}; '
-                f'Длительность: {self.duration:.3f} ч.; '
-                f'Дистанция: {self.distance:.3f} км; '
-                f'Ср. скорость: {self.speed:.3f} км/ч; '
-                f'Потрачено ккал: {self.calories:.3f}.')
+        return self.INFO.format(**asdict(self))
 
 
 class Training:
